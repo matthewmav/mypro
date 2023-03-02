c8c0a8248621574f527e9ce01477ea3b0055fb39
matthewwmav <george.solymosi@gmail.com>
2023-03-02 03:20:16 +0100

    Initial

---
+++ src/mul.py
@@ -0,0 +1,5 @@
+def mulxy(x, y):
+    return x * y 
+
+def addxy(x, y):
+    return x + y 
---
+++ tests/test_fixture_demo.py
@@ -0,0 +1,18 @@
+# fixture_demo.py
+
+import pytest
+from ..src import mul as m
+
+@pytest.fixture
+def example_fixture():
+    return 2
+
+@pytest.fixture
+def example_fixture2():
+    return 15
+
+def test_with_fixture(example_fixture):
+    assert example_fixture == 2
+
+def test_mul(example_fixture, example_fixture2):
+    assert m.mulxy(example_fixture, example_fixture2) == 30
